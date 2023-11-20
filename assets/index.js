2711a0739448041aed1b8cba442f6b2bd649b1c3
Bundana <bundanaabdulhafiz@gmail.com>
2023-11-20 09:29:09 +0000

    added js in index.js

--- assets/index.js
+++ assets/index.js
@@ -1,36 +1,46 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const burgerMenu = document.querySelector('.burger-menu');
-  const navMenu = document.querySelector('.menu');
+'use strict';
 
-  burgerMenu.addEventListener('click', function () {
-      navMenu.classList.toggle('active');
+
+
+/**
+ * navbar toggle
+ */
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
   });
-});
-document.addEventListener('DOMContentLoaded', function () {
-  /**
-   * Represents a Swiper instance.
-   * @class
-   * @param {string|HTMLElement} container - The container element or selector for the Swiper.
-   * @param {Object} options - The options for configuring the Swiper.
-   * @param {number} options.slidesPerView - The number of slides to display per view.
-   * @param {number} options.spaceBetween - The space between slides.
-   * @param {Object} options.navigation - The navigation options for the Swiper.
-   * @param {string|HTMLElement} options.navigation.nextEl - The element or selector for the next button.
-   * @param {string|HTMLElement} options.navigation.prevEl - The element or selector for the previous button.
-   * @param {Object} options.pagination - The pagination options for the Swiper.
-   * @param {string|HTMLElement} options.pagination.el - The element or selector for the pagination container.
-   * @param {boolean} options.pagination.clickable - Whether the pagination is clickable.
-   */
-  const swiper = new Swiper('.swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-      },
+}
+
+/**
+ * toggle navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
   });
+}
+
+
+
+
+
+/**
+ * header active when window scrolled down
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 50 ? header.classList.add("active")
+    : header.classList.remove("active");
 });
